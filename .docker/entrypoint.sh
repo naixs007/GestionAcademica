603c8be93733dde05332e7b646bfa5ee21bea61c
Bryan <b.a.h.007@live.com>
2025-11-15 10:38:26 -0400

    cargado de seeder

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -9,6 +9,10 @@ echo "Running entrypoint script..."
 echo "Running database migrations..."
 php artisan migrate --force
 
+# 2. Ejecutar los seeders (cargar datos iniciales)
+echo "Running database seeders..."
+php artisan db:seed --force
+
 # Optimizar la aplicación para producción
 echo "Caching configuration and routes..."
 php artisan config:cache
